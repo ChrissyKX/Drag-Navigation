251f827738d88897350994533fb6169908e8de7d
ChrissyKX <47626703+ChrissyKX@users.noreply.github.com>
2020-01-01 02:21:28 +0800

    Initial Commit

---
+++ Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Practice
+//
+//  Created by Xiyu Zhang on 1/1/20.
+//  Copyright © 2020 Xiyu Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
